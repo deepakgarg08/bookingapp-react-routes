aec312bf2e471a457557cf2842d9669373a47287
Deepak <erdkgarg08@gmail.com>
2020-06-03 10:46:00 +0200

    optimised confirmaiton comp

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -5,70 +5,49 @@ import * as actions from '../redux/actionTypes'
 class Confirmation extends React.Component {
 
     render() {
-
-
-
-
         return (
             <div id="veryouter">
                 <div id="outerdiv">
-                    <div id="innerdivconfirmaiton">
-                        <div id="friendlyprops">
-                            <Friendly flag={this.props.init} id="friendly1" />
-                        </div>
+                    <div id="friendlyprops">
+                        <Friendly flag={this.props.init} id="friendly1" />
+                    </div>
+                    <div>
                         <div>
                             <Friendly flag={this.props.init} id="friendly2" />
                         </div>
                         <div>
                             <Friendly flag={this.props.init} id="friendly3" />
                         </div>
-                        <div id="friendlyprops">
-                            <Friendly flag={this.props.init} id="friendly4" />
-                        </div>
                     </div>
+                    <div id="friendlyprops">
+                        <Friendly flag={this.props.init} id="friendly4" />
+                    </div>
+
                 </div>
-
-                {/* <Friendly flag = 'b'/>
-                <Friendly flag = 'c'/> */}
-
             </div>
         )
     }
 }
 
-
-
-
 function Friendly(props) {
 
     console.log('props.............', props.flag.dateAndTime.date)
 
-
-    let handleEvent = (props) => {
-    }
-
     return (
 
-        <div>
-            <span>
-                <button id={props.id} onClick={handleEvent}>
-                    <div>
-                        <div style={{ float: "right"}}>
-                            <span style={{ color: "#6C70DC", fontFamily : 'Poppins' }}>Location: <span> </span></span>
-                            <span>  {props.flag.location} </span>
-                        </div>
-                        <div style={{ float: "left"}}>
-                            <span style={{ color: "#6C70DC" , fontFamily : 'Poppins'}}>Date & TIme: <span> </span></span>
-                            <span>  {props.flag.dateAndTime.date} </span>
-                        </div>
-                       
-                    </div>
-                </button>
-            </span>
+        <div id={props.id}>
+
+            <div style={{ float: "right" }}>
+                <span style={{ color: "#6C70DC", fontFamily: 'Poppins' }}>Location: <span> </span></span>
+                <span>  {props.flag.location} </span>
+            </div>
+            <div style={{ float: "left" }}>
+                <span style={{ color: "#6C70DC", fontFamily: 'Poppins' }}>Date & TIme: <span> </span></span>
+                <span>  {props.flag.dateAndTime.date} </span>
+            </div>
+
         </div>
     );
-
-
 
     //avatar complex component
     function Avatar(props) {
