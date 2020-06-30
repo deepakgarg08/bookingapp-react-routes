7a7def6211566d99400ff0926e38e14ad30925dc
Deepak <erdkgarg08@gmail.com>
2020-06-30 21:48:52 +0200

    left container component added

--- src/components/LeftContainerComponent.js
+++ src/components/LeftContainerComponent.js
@@ -49,47 +49,137 @@ function LeftContainerComponent(props) {
     let currentpagestate = props.init.currentPageState
     //this is for css
     const currentState = returnState(currentpagestate);
+    console.log('currentState', currentState)
     return (
-        
-        <div id="leftsidecontainer" >
-                {/* <Link className="brand-logo" to="/" onClick={() => (props.changecurrentPageState('location'))}> Booking App</Link> */}
 
-            <div id="leftcontainerdata" className="leftcontdata">
+        //     {/* <Link className="brand-logo" to="/" onClick={() => (props.changecurrentPageState('location'))}> Booking App</Link> */}
+        // <div id="leftsidecontainer" >
 
-                <div className="nameproperty">
-                    <span className={currentTab(currentState, 1)}>1</span>
-                    <span className={currentHeading(currentState, 1)}> <NavLink exact to="/" onClick={() => (props.changecurrentPageState('location'))}>Location </NavLink> </span>
-                </div>
-                <div className="nameproperty">
-                    <span className={currentTab(currentState, 2)}>2</span>
-                    <span className={currentHeading(currentState, 2)}><NavLink to="/" onClick={() => (props.changecurrentPageState('staff'))} > staff</NavLink> </span>
-                </div>
-                <div className="nameproperty">
-                    <span className={currentTab(currentState, 3)}>3</span>
-                    <span className={currentHeading(currentState, 3)}> <NavLink to="/" onClick={() => (props.changecurrentPageState('services'))} >  Services </NavLink> </span>
-                </div>
-                <div className="nameproperty">
-                    <span className={currentTab(currentState, 4)}>4</span>
-                    <span className={currentHeading(currentState, 4)} > <NavLink to="/" onClick={() => (props.changecurrentPageState('extraService'))} >  Service Extras </NavLink> </span>
-                </div>
+        //     <div id="leftcontainerdata" className="leftcontdata">
 
-                <div className="nameproperty">
-                    <span className={currentTab(currentState, 5)}>5</span>
-                    <span className={currentHeading(currentState, 5)}> <NavLink to="/" onClick={() => (props.changecurrentPageState('dateAndTime'))} >  Date and Time </NavLink> </span>
-                </div>
+        //         <div className="nameproperty">
+        //             <span className={currentTab(currentState, 1)}>1</span>
+        //             <span className={currentHeading(currentState, 1)}>
+        //                 <a className="nav-link" href="index.html"> Location </a>
+        //                 <NavLink exact to="/" onClick={() => (props.changecurrentPageState('location'))}>Location </NavLink>
+        //             </span>
+        //         </div>
+        //         <div className="nameproperty">
+        //             <span className={currentTab(currentState, 2)}>2</span>
+        //             <span className={currentHeading(currentState, 2)}>
+        //                 <NavLink to="/" onClick={() => (props.changecurrentPageState('staff'))} > staff</NavLink>
+        //             </span>
+        //         </div>
+        //         <div className="nameproperty">
+        //             <span className={currentTab(currentState, 3)}>3</span>
+        //             <span className={currentHeading(currentState, 3)}>
+        //                 <NavLink to="/" onClick={() => (props.changecurrentPageState('services'))} >  Services </NavLink>
+        //             </span>
+        //         </div>
+        //         <div className="nameproperty">
+        //             <span className={currentTab(currentState, 4)}>4</span>
+        //             <span className={currentHeading(currentState, 4)} >
+        //                 <NavLink to="/" onClick={() => (props.changecurrentPageState('extraService'))} >  Service Extras </NavLink>
+        //             </span>
+        //         </div>
 
-                <div className="nameproperty">
-                    <span className={currentTab(currentState, 6)}>6</span>
-                    <span className={currentHeading(currentState, 6)}>  <NavLink to="/" onClick={() => (props.changecurrentPageState('information'))} > Information </NavLink> </span>
-                </div>
+        //         <div className="nameproperty">
+        //             <span className={currentTab(currentState, 5)}>5</span>
+        //             <span className={currentHeading(currentState, 5)}>
+        //                 <NavLink to="/" onClick={() => (props.changecurrentPageState('dateAndTime'))} >  Date and Time </NavLink>
+        //             </span>
+        //         </div>
 
-                <div className="nameproperty">
+        //         <div className="nameproperty">
+        //             <span className={currentTab(currentState, 6)}>6</span>
+        //             <span className={currentHeading(currentState, 6)}>
+        //                 <NavLink to="/" onClick={() => (props.changecurrentPageState('information'))} > Information </NavLink>
+        //             </span>
+        //         </div>
+
+        //         <div className="nameproperty">
+        //             <span className={currentTab(currentState, 7)}>7</span>
+        //             <span className={currentHeading(currentState, 7)}>
+        //                 <NavLink to="/" onClick={() => (props.changecurrentPageState('confirmation'))} > Confirmation </NavLink>
+        //             </span>
+        //         </div>
+        //         <div className="nameproperty">
+        //             {/* <span className={currentTab(currentState, 8)}></span> */}
+        //             <span className={currentHeading(currentState, 8)}>  </span>
+        //         </div>
+        //     </div>
+        // </div>
+
+
+
+        <div className="d-flex flex-column row-hl">
+
+            <div className="navbar navbar-expand-sm mb-3">
+                <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+                    <span className="navbar-toggler-icon">
+                        <i className="fas fa-bars" style={{ "color": "#fff", "fontSize": "28px" }}></i>
+                    </span>
+                </button>
+
+                {/* <div className="nameproperty">
                     <span className={currentTab(currentState, 7)}>7</span>
-                    <span className={currentHeading(currentState, 7)}>  <NavLink to="/" onClick={() => (props.changecurrentPageState('confirmation'))} > Confirmation </NavLink> </span>
-                </div>
-                <div className="nameproperty">
-                    {/* <span className={currentTab(currentState, 8)}></span> */}
-                    <span className={currentHeading(currentState, 8)}>  </span>
+                    <span className={currentHeading(currentState, 7)}>
+                        <NavLink to="/" onClick={() => (props.changecurrentPageState('confirmation'))} > Confirmation </NavLink>
+                    </span>
+                </div> */}
+
+
+                <div className="collapse navbar-collapse nav-padding" id="navbarNav">
+                    <ul className="navbar-nav flex-column nav-prop">
+
+                        <li className="nav-item nameproperty">
+                            <span id="myclass" className={currentTab(currentState, 1)} > 1</span>
+                            <span className={currentHeading(currentState, 1)}>
+                                <NavLink  className="nav-link"  exact to="/" onClick={() => (props.changecurrentPageState('location'))}>Location </NavLink>
+                            </span>
+                        </li>
+
+                        <li className="nav-item nameproperty">
+                            <span id="myclass" className={currentTab(currentState, 2)} > 2 </span>
+                            <span className={currentHeading(currentState, 2)}>
+                                <NavLink to="/" className="nav-link" onClick={() => (props.changecurrentPageState('staff'))} > staff</NavLink>
+                            </span>
+                        </li>
+
+                        <li className="nav-item nameproperty">
+                            <span id="myclass" className={currentTab(currentState, 3)} > 3 </span>
+                            <span className={currentHeading(currentState, 3)}>
+                                <NavLink to="/" className="nav-link" onClick={() => (props.changecurrentPageState('services'))} >  Services </NavLink>
+                            </span>
+                        </li>
+                        <li className="nav-item nameproperty">
+                            <span id="myclass" className={currentTab(currentState, 4)} > 4 </span>
+                            <span className={currentHeading(currentState, 4)}>
+                                <NavLink to="/" className="nav-link" onClick={() => (props.changecurrentPageState('extraService'))} > Extras </NavLink>
+                            </span>
+                        </li>
+                        <li className="nav-item nameproperty ">
+                            <span id="myclass" className={currentTab(currentState, 5)} > 5 </span>
+                            <span className={currentHeading(currentState, 5)}>
+                                <NavLink to="/" className="nav-link" onClick={() => (props.changecurrentPageState('dateAndTime'))} >  Date and Time </NavLink>
+                            </span>
+                        </li>
+                        <li className="nav-item nameproperty">
+                            <span id="myclass" className={currentTab(currentState, 6)} > 6 </span>
+                            <span className={currentHeading(currentState, 6)}>
+                                <NavLink to="/" className="nav-link" onClick={() => (props.changecurrentPageState('information'))} > Information </NavLink>
+                            </span>
+                        </li>
+                        <li className="nav-item nameproperty">
+                            <span id="myclass" className={currentTab(currentState, 7)} > 7 </span>
+                            <span className={currentHeading(currentState, 7)}>
+                                <NavLink to="/" className="nav-link" onClick={() => (props.changecurrentPageState('confirmation'))} > Confirmation </NavLink>
+                            </span>
+                        </li>
+                        <li className="nav-item nameproperty">
+                            <a className="nav-link" href="#"></a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,19 +3,40 @@ import RightContainerComponent from './RightContainer'
 import Navbar from './Navbar'
 function MainPage() {
     return (
-        <div className="MainPage">
-            <h1 id="h1"> Welcome to booking app</h1>
-            <div id="Wrapper">
-                <div id="container">
-                    <Navbar />
-                    <div id="rightsidecontainer">
-                        <div id="rightcontainerdata">
-                            <RightContainerComponent />
+        // <div className="MainPage">
+        //     <h1 id="h1"> Welcome to booking app</h1>
+        //     <div id="Wrapper">
+        //         <div id="container">
+        //             <Navbar />
+        //             <div id="rightsidecontainer">
+        //                 <div id="rightcontainerdata">
+        //                     <RightContainerComponent />
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+
+        <div className="d-flex mx-auto">
+    
+            <div className="container container-fluid">
+                <h2 className="text-center my-3 text-warning">Welcome to Booking App</h2>
+
+                <div className="row no-gutters">
+                    <div className="col-sm-3 col-md-3 col-lg-3 col-xl-3 text-left leftcontainer bg-black">
+                        {/* leftcontainercomponent */}
+                        <Navbar />
+                        <div className="col-sm-9 col-md-9 col-lg-9 col-xl-9 h-100">
+                        {/* RIghtcontainercomponent */}
+                        <RightContainerComponent />
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        
+
     );
 }
 
