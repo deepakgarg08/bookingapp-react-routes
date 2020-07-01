df73a2b1c56dd9d38116112360bbae445f5814e1
Deepak <erdkgarg08@gmail.com>
2020-07-02 01:04:13 +0200

    random images code commented, not working in heroku

--- src/components/Location.js
+++ src/components/Location.js
@@ -10,17 +10,24 @@ class Location extends React.Component {
             // { name: "Bhiwani", img: "image4.jpg", id: 4 },
             // { name: "Delhi", img: "image5.jpg", id: 5 },
             // { name: "Hisar", img: "image5.jpg", id: 6 }
+
+            { name: "Paderborn", img: "image1.jpg", id: 1 },
+            { name: "Bielefeld", img: "image2.jpg", id: 2 },
+            { name: "Frankfurt", img: "image3.jpg", id: 3 },
+            { name: "Delhi", img: "image4.jpg", id: 4 },
+            { name: "Rohtak", img: "image5.jpg", id: 5 },
+            { name: "Mumbai", img: "image5.jpg", id: 6 }
             
             // { name: "Add new City", img: "image6.jpg", id: 6 }
 
             //comment below code
 
-            { name: "Paderborn", img: "https://source.unsplash.com/random/200x200", id: 1 },
-            { name: "Bielefeld", img: "https://source.unsplash.com/random/201x200", id: 2 },
-            { name: "Frankfurt", img: "https://source.unsplash.com/random/200x202", id: 3 },
-            { name: "Delhi",     img: "https://source.unsplash.com/random/202x200", id: 4 },
-            { name: "Rohtak",    img: "https://source.unsplash.com/random/200x201", id: 5 },
-            { name: "Mumbai",    img: "https://source.unsplash.com/random/203x200", id: 6 }
+            // { name: "Paderborn", img: "https://source.unsplash.com/random/200x200", id: 1 },
+            // { name: "Bielefeld", img: "https://source.unsplash.com/random/201x200", id: 2 },
+            // { name: "Frankfurt", img: "https://source.unsplash.com/random/200x202", id: 3 },
+            // { name: "Delhi",     img: "https://source.unsplash.com/random/202x200", id: 4 },
+            // { name: "Rohtak",    img: "https://source.unsplash.com/random/200x201", id: 5 },
+            // { name: "Mumbai",    img: "https://source.unsplash.com/random/203x200", id: 6 }
         ]
     }
     // https://source.unsplash.com/random/200x200
--- src/components/Services.js
+++ src/components/Services.js
@@ -5,19 +5,19 @@ class Services extends React.Component {
     serviceData = {
         serviceProps: [
 
-            // { name: "Eureka Forbes", img: "water1.jpg", price: "100", id: 1 },
-            // { name: "Aquaguard", img: "geyser.jpg", price: "500", id: 2 },
-            // { name: "Reve Osmosis (RO)", img: "water1.jpg", price: "1000", id: 3 },
-            // { name: "Kent (RO)", img: "water1.jpg", price: "2000", id: 4 },
-            // { name: "Aquasure (RO)", img: "water1.jpg", price: "500", id: 5 },
-            // { name: "water Geyser", img: "geyser.jpg", price: "5000", id: 6 },
+            { name: "Eureka Forbes", img: "water1.jpg", price: "100", id: 1 },
+            { name: "Aquaguard", img: "geyser.jpg", price: "500", id: 2 },
+            { name: "Reve Osmosis (RO)", img: "water1.jpg", price: "1000", id: 3 },
+            { name: "Kent (RO)", img: "water1.jpg", price: "2000", id: 4 },
+            { name: "Aquasure (RO)", img: "water1.jpg", price: "500", id: 5 },
+            { name: "water Geyser", img: "geyser.jpg", price: "5000", id: 6 }
 
-            { name: "Eureka Forbes",      img: "https://source.unsplash.com/random/200x210", price: "100", id: 1 },
-            { name: "Aquaguard",          img: "https://source.unsplash.com/random/201x211", price: "500", id: 2 },
-            { name: "Reve Osmosis (RO)",  img: "https://source.unsplash.com/random/200x212", price: "1000", id: 3 },
-            { name: "Kent (RO)",          img: "https://source.unsplash.com/random/202x213", price: "2000", id: 4 },
-            { name: "Aquasure (RO)",      img: "https://source.unsplash.com/random/200x214", price: "500", id: 5 },
-            { name: "water Geyser",       img: "https://source.unsplash.com/random/203x215", price: "5000", id: 6 },
+            // { name: "Eureka Forbes",      img: "https://source.unsplash.com/random/200x210", price: "100", id: 1 },
+            // { name: "Aquaguard",          img: "https://source.unsplash.com/random/201x211", price: "500", id: 2 },
+            // { name: "Reve Osmosis (RO)",  img: "https://source.unsplash.com/random/200x212", price: "1000", id: 3 },
+            // { name: "Kent (RO)",          img: "https://source.unsplash.com/random/202x213", price: "2000", id: 4 },
+            // { name: "Aquasure (RO)",      img: "https://source.unsplash.com/random/200x214", price: "500", id: 5 },
+            // { name: "water Geyser",       img: "https://source.unsplash.com/random/203x215", price: "5000", id: 6 },
         ]
     }
 
